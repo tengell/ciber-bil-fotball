a0e7e70e15e83b1d2873b32435b7220b1b84b439
tengell <trond.engell@gmail.com>
2014-10-24 09:51:48 +0200

    Addded jquery and bootstrap. Index will now show the list of current players.

--- src/main/java/ciber/bil/fotball/rest/HelloController.java
+++ src/main/java/ciber/bil/fotball/rest/HelloController.java
@@ -1,19 +1,23 @@
 package ciber.bil.fotball.rest;
 
+import ciber.bil.fotball.model.entities.Person;
+import ciber.bil.fotball.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
+	@Autowired
+	PersonService personService;
+
 	@RequestMapping("/")
-	public String index(){
+	public String index(Model model){
+		Iterable<Person> personList = personService.findAll();
+		model.addAttribute("persons", personList);
 		return "index";
 	}
-
-//	@RequestMapping("/addPerson")
-//	public String addPerson(){
-//		return "addPerson";
-//	}
 
 }
--- src/main/java/ciber/bil/fotball/rest/PersonController.java
+++ src/main/java/ciber/bil/fotball/rest/PersonController.java
@@ -8,9 +8,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.time.LocalDate;
 import java.util.Optional;
 
@@ -21,61 +23,60 @@ public class PersonController {
 	@Autowired
 	PersonService personService;
 
-//	@Value("${defaultHelloName}")
-//	private String defaultHelloName;
-//
-//	@RequestMapping("/hello")
-//	public String helloWorld(){ return "Hello " + defaultHelloName; }
-
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
 	public Iterable<Person> allPersons() {
 		return personService.findAll();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Person> person(@PathVariable String id){
+	public ResponseEntity<Person> person(@PathVariable String id) {
 		Optional<Person> person = personService.find(Long.valueOf(id));
-		
-		if(person.isPresent()){
+
+		if (person.isPresent()) {
 			return new ResponseEntity<Person>(person.get(), HttpStatus.OK);
-		}else{
+		} else {
 			return new ResponseEntity<Person>(HttpStatus.NOT_FOUND);
 		}
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Person> createPerson(
+	public ResponseEntity<Person> savePersonAndReturnPersonWithLocationInHttpHeader(
 			@RequestParam(value = "firstName", required = true) String firstName,
 			@RequestParam(value = "lastName", required = true) String lastName,
 			@RequestParam(value = "dateOfBirth", required = false) String dateOfBirth,
 			UriComponentsBuilder builder
 	) {
-		Person personToInsert = LambdaPersonBuilder.build(p -> {
-			p.setFirstName(firstName);
-			p.setLastName(lastName);
-			p.setDateOfBirth(
-					Optional.ofNullable(dateOfBirth)
-							.map(LocalDate::parse)
-			);
-		});
-		
-		Person storedPerson = personService.save(personToInsert);
+		Person personToSave = createPerson(firstName, lastName, dateOfBirth);
+		Person savedPerson = personService.save(personToSave);
 
 		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(builder.path("/persons/{id}")
+				.buildAndExpand(savedPerson.getId())
+				.toUri());
 
-		headers.setLocation(
-				builder.path("/persons/{id}")
-						.buildAndExpand(storedPerson.getId())
-						.toUri()
-		);
-
-		return new ResponseEntity<Person>(storedPerson, headers, HttpStatus.CREATED);
+		return new ResponseEntity<Person>(savedPerson, headers, HttpStatus.CREATED);
 	}
-	
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE) 
-	public ResponseEntity<Person> delete(@PathVariable String id){
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String savePersonAndReturnToIndexWithAllPersons(
+			@RequestParam(value = "firstName", required = true) String firstName,
+			@RequestParam(value = "lastName", required = true) String lastName,
+			@RequestParam(value = "dateOfBirth", required = false) String dateOfBirth,
+			Model model
+	) {
+		Person personToSave = createPerson(firstName, lastName, dateOfBirth);
+		personService.save(personToSave);
+
+		model.addAttribute("persons", allPersons());
+		
+		return "index";
+	}
+
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Person> delete(@PathVariable String id) {
 
 	/*	try {
 			personService.delete(Long.valueOf(id));
@@ -84,9 +85,20 @@ public class PersonController {
 		}
 		if(personDeleted){*/
 		personService.delete(Long.valueOf(id));
-			return new ResponseEntity<Person>(HttpStatus.OK);
+		return new ResponseEntity<Person>(HttpStatus.OK);
 //		}else{
 //			return new ResponseEntity<Person>(HttpStatus.NOT_FOUND);
 //		}
+	}
+
+	private Person createPerson(String firstName, String lastName, String dateOfBirth) {
+		return LambdaPersonBuilder.build(p -> {
+			p.setFirstName(firstName);
+			p.setLastName(lastName);
+			p.setDateOfBirth(
+					Optional.ofNullable(dateOfBirth)
+							.map(LocalDate::parse)
+			);
+		});
 	}
 }
